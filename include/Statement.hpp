0c5ae189d732e9abd0a148b154e65a6cff341c5f
cmh <chenmh25@sjtu.edu.cn>
2025-11-25 23:20:31 +0800

    Sequential Statements

--- include/Statement.hpp
+++ include/Statement.hpp
@@ -22,4 +22,59 @@ class Statement {
 };
 
 // TODO: Other statement types derived from Statement, e.g., GOTOStatement,
+
+
 // LetStatement, etc.
+class LetStatement : public Statement {
+public:
+  LetStatement(std::string source, std::string var, Expression* expr);
+  ~LetStatement() override;
+  void execute(VarState& state, Program& program) const override;
+
+private:
+  std::string var_;
+  Expression* expr_;
+};
+
+
+// PrintStatement
+class PrintStatement : public Statement {
+public:
+  PrintStatement(std::string source, Expression* expr);
+  ~PrintStatement() override;
+  void execute(VarState& state, Program& program) const override;
+
+private:
+  std::string var_;
+  Expression* expr_;
+};
+
+
+// RemStatement
+class RemStatement : public Statement {
+public:
+  RemStatement(std::string source);
+  ~RemStatement() override = default;
+  void execute(VarState& state, Program& program) const override;
+};
+
+
+// EndStatement
+class EndStatement : public Statement {
+public:
+  EndStatement(std::string source);
+  ~EndStatement() override = default;
+  void execute(VarState& state, Program& program) const override;
+};
+
+
+// InputStatement
+class InputStatement : public Statement {
+public:
+  InputStatement(std::string source, std::string var);
+  ~InputStatement() override = default;
+  void execute(VarState& state, Program& program) const override;
+
+private:
+  std::string var_;
+}
